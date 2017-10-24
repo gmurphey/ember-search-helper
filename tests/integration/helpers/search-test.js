9abff311f679200d51b02df736a221227c9eae35
Garrett Murphey <gmurphey@gmurphey.com>
2017-10-24 11:03:27 +0900

    Converting to new style of tests.

--- tests/integration/helpers/search-test.js
+++ tests/integration/helpers/search-test.js
@@ -1,191 +1,194 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from 'ember-test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+
 import { get, set } from '@ember/object';
 import { A as emberArray } from '@ember/array';
 import { run } from '@ember/runloop';
 import { findAll, find, fillIn } from 'ember-native-dom-helpers';
 
-moduleForComponent('search', 'Integration | Helper | search', {
-  integration: true
-});
+module('search', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it returns the collection if the query is empty', function(assert) {
-  set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
+  test('it returns the collection if the query is empty', async function(assert) {
+    set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
 
-  this.render(hbs`
-    <ul>
-    {{#each (search "" collection) as |result|}}
-      <li>{{result}}</li>
-    {{/each}}
-    </ul>
-  `);
-
-  let results = findAll('ul li');
-
-  assert.equal(results.length, 3);
-  assert.equal(results[0].textContent.trim(), 'apples');
-  assert.equal(results[1].textContent.trim(), 'bananas');
-  assert.equal(results[2].textContent.trim(), 'oranges');
-});
-
-test('it can have zero results', function(assert) {
-  set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
-
-  this.render(hbs`
-    {{#unless (search "kirby" collection)}}
-      <p>No results.</p>
-    {{/unless}}
-  `);
-
-  assert.equal(find('p').textContent.trim(), 'No results.');
-});
-
-test('it renders a searched state', function(assert) {
-  set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
-
-  this.render(hbs`
-    <ul>
-    {{#each (search "Apples" collection) as |result|}}
-      <li>{{result}}</li>
-    {{/each}}
-    </ul>
-  `);
-
-  assert.equal(findAll('ul li').length, 1);
-  assert.equal(find('ul li').textContent.trim(), 'apples');
-});
-
-test('the results can match partial queries', function(assert) {
-  set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
-
-  this.render(hbs`
-    <ul>
-    {{#each (search "GES" collection) as |result|}}
-      <li>{{result}}</li>
-    {{/each}}
-    </ul>
-  `);
-
-  assert.equal(findAll('ul li').length, 1);
-  assert.equal(find('ul li').textContent.trim(), 'oranges');
-});
-
-test('the results can be forced to match the case of the query', function(assert) {
-  set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
-
-  this.render(hbs`
-    {{#unless (search "GES" collection caseSensitive=true)}}
-      <p>No results.</p>
-    {{/unless}}
-  `);
-
-  assert.equal(find('p').textContent.trim(), 'No results.');
-});
-
-test('the results change when the query changes', async function(assert) {
-  set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
-  set(this, 'query', 'apples');
-
-  this.render(hbs`
-    {{input value=query on-key-press=(action (mut query))}}
-
-    <ul>
-      {{#each (search query collection) as |result|}}
+    await render(hbs`
+      <ul>
+      {{#each (search "" collection) as |result|}}
         <li>{{result}}</li>
       {{/each}}
-    </ul>
-  `);
+      </ul>
+    `);
 
-  assert.equal(findAll('ul li').length, 1);
-  assert.equal(find('ul li').textContent.trim(), 'apples');
+    let results = findAll('ul li');
 
-  await fillIn('input', 'bananas');
+    assert.equal(results.length, 3);
+    assert.equal(results[0].textContent.trim(), 'apples');
+    assert.equal(results[1].textContent.trim(), 'bananas');
+    assert.equal(results[2].textContent.trim(), 'oranges');
+  });
 
-  assert.equal(findAll('ul li').length, 1);
-  assert.equal(find('ul li').textContent.trim(), 'bananas');
-});
+  test('it can have zero results', async function(assert) {
+    set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
 
-test('the results can be forced to be an exact match of the query', function(assert) {
-  set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
-  set(this, 'query', 'app');
+    await render(hbs`
+      {{#unless (search "kirby" collection)}}
+        <p>No results.</p>
+      {{/unless}}
+    `);
 
-  this.render(hbs`
-    <ul>
-      {{#each (search query collection exactMatch=true) as |result|}}
+    assert.equal(find('p').textContent.trim(), 'No results.');
+  });
+
+  test('it renders a searched state', async function(assert) {
+    set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
+
+    await render(hbs`
+      <ul>
+      {{#each (search "Apples" collection) as |result|}}
         <li>{{result}}</li>
       {{/each}}
-    </ul>
-  `);
+      </ul>
+    `);
 
-  assert.equal(findAll('ul li').length, 0);
+    assert.equal(findAll('ul li').length, 1);
+    assert.equal(find('ul li').textContent.trim(), 'apples');
+  });
 
-  run(() => {
+  test('the results can match partial queries', async function(assert) {
+    set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
+
+    await this.render(hbs`
+      <ul>
+      {{#each (search "GES" collection) as |result|}}
+        <li>{{result}}</li>
+      {{/each}}
+      </ul>
+    `);
+
+    assert.equal(findAll('ul li').length, 1);
+    assert.equal(find('ul li').textContent.trim(), 'oranges');
+  });
+
+  test('the results can be forced to match the case of the query', async function(assert) {
+    set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
+
+    await this.render(hbs`
+      {{#unless (search "GES" collection caseSensitive=true)}}
+        <p>No results.</p>
+      {{/unless}}
+    `);
+
+    assert.equal(find('p').textContent.trim(), 'No results.');
+  });
+
+  test('the results change when the query changes', async function(assert) {
+    set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
     set(this, 'query', 'apples');
+
+    await render(hbs`
+      {{input value=query on-key-press=(action (mut query))}}
+
+      <ul>
+        {{#each (search query collection) as |result|}}
+          <li>{{result}}</li>
+        {{/each}}
+      </ul>
+    `);
+
+    assert.equal(findAll('ul li').length, 1);
+    assert.equal(find('ul li').textContent.trim(), 'apples');
+
+    await fillIn('input', 'bananas');
+
+    assert.equal(findAll('ul li').length, 1);
+    assert.equal(find('ul li').textContent.trim(), 'bananas');
   });
 
-  assert.equal(findAll('ul li').length, 1);
-  assert.equal(find('ul li').textContent.trim(), 'apples');
-});
+  test('the results can be forced to be an exact match of the query', async function(assert) {
+    set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
+    set(this, 'query', 'app');
 
-test('properties can be an array of synchronous props', async function(assert) {
-  set(this, 'collection', emberArray([
-    {
-      name: "apples",
-      opinion: "okay"
-    },
-    {
-      name: "bananas",
-      opinion: "gross"
-    },
-    {
-      name: "oranges",
-      opinion: "awesome"
-    }
-  ]));
-  set(this, 'properties', emberArray(['name', 'opinion']));
+    await render(hbs`
+      <ul>
+        {{#each (search query collection exactMatch=true) as |result|}}
+          <li>{{result}}</li>
+        {{/each}}
+      </ul>
+    `);
 
-  set(this, 'query', 'apples');
+    assert.equal(findAll('ul li').length, 0);
 
-  this.render(hbs`
-    {{input value=query on-key-press=(action (mut query))}}
+    run(() => {
+      set(this, 'query', 'apples');
+    });
 
-    <ul>
-      {{#each (search query collection properties=properties) as |result|}}
-        <li>{{result.name}} ({{result.opinion}})</li>
+    assert.equal(findAll('ul li').length, 1);
+    assert.equal(find('ul li').textContent.trim(), 'apples');
+  });
+
+  test('properties can be an array of synchronous props', async function(assert) {
+    set(this, 'collection', emberArray([
+      {
+        name: "apples",
+        opinion: "okay"
+      },
+      {
+        name: "bananas",
+        opinion: "gross"
+      },
+      {
+        name: "oranges",
+        opinion: "awesome"
+      }
+    ]));
+    set(this, 'properties', emberArray(['name', 'opinion']));
+
+    set(this, 'query', 'apples');
+
+    await render(hbs`
+      {{input value=query on-key-press=(action (mut query))}}
+
+      <ul>
+        {{#each (search query collection properties=properties) as |result|}}
+          <li>{{result.name}} ({{result.opinion}})</li>
+        {{/each}}
+      </ul>
+    `);
+
+    assert.equal(findAll('ul li').length, 1);
+    assert.equal(find('ul li').textContent.trim(), 'apples (okay)');
+
+    await fillIn('input', 'awesome');
+
+    assert.equal(findAll('ul li').length, 1);
+    assert.equal(find('ul li').textContent.trim(), 'oranges (awesome)');
+  });
+
+  test('it updates the results if the collection is updated', async function(assert) {
+    set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
+
+    await render(hbs`
+      <ul>
+      {{#each (search "" collection) as |result|}}
+        <li>{{result}}</li>
       {{/each}}
-    </ul>
-  `);
+      </ul>
+    `);
 
-  assert.equal(findAll('ul li').length, 1);
-  assert.equal(find('ul li').textContent.trim(), 'apples (okay)');
+    let results = findAll('ul li');
 
-  await fillIn('input', 'awesome');
+    assert.equal(results.length, 3);
+    assert.equal(results[0].textContent.trim(), 'apples');
+    assert.equal(results[1].textContent.trim(), 'bananas');
+    assert.equal(results[2].textContent.trim(), 'oranges');
 
-  assert.equal(findAll('ul li').length, 1);
-  assert.equal(find('ul li').textContent.trim(), 'oranges (awesome)');
-});
+    run(() => {
+      get(this, 'collection').pushObject('mangoes');
+    });
 
-test('it updates the results if the collection is updated', function(assert) {
-  set(this, 'collection', emberArray(['apples', 'bananas', 'oranges']));
-
-  this.render(hbs`
-    <ul>
-    {{#each (search "" collection) as |result|}}
-      <li>{{result}}</li>
-    {{/each}}
-    </ul>
-  `);
-
-  let results = findAll('ul li');
-
-  assert.equal(results.length, 3);
-  assert.equal(results[0].textContent.trim(), 'apples');
-  assert.equal(results[1].textContent.trim(), 'bananas');
-  assert.equal(results[2].textContent.trim(), 'oranges');
-
-  run(() => {
-    get(this, 'collection').pushObject('mangoes');
+    assert.equal(findAll('ul li').length, 4);
   });
-
-  assert.equal(findAll('ul li').length, 4);
 });
